c122aa1231325328c8e9a966e202cb11c78f51c8
mateogic <ljc_2318516761@163.com>
2025-05-19 19:06:01 +0800

    响应式布局

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './assets/main.css'
-
+// 导入 Element Plus 图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import { createApp } from 'vue'
 import App from '@/App.vue'
 // 导入路由
@@ -10,3 +11,7 @@ const app = createApp(App)
 // 应用路由
 app.use(router)
 app.mount('#app')
+// 引入图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
