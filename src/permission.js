aa2dd3420e866a7bbf46c61235f6e0f44f495bc0
mateogic <ljc_2318516761@163.com>
2025-05-27 23:02:01 +0800

    重复登录问题优化、密码框可显示密码

--- src/permission.js
+++ src/permission.js
@@ -1,6 +1,6 @@
 import router from '@/router/index'
 import { getToken } from '@/composables/auth'
-import { showPageLoading, hidePageLoading } from '@/composables/util'
+import { showPageLoading, hidePageLoading, showMessage } from '@/composables/util'
 
 // 全局路由前置守卫
 router.beforeEach((to, from, next) => {
@@ -13,10 +13,14 @@ router.beforeEach((to, from, next) => {
     if (!token && to.path.startsWith('/admin')) {
         showMessage('请先登录', 'warning')
         next({ path: '/login' })
+    } else if (token && to.path == '/login') {
+        // 若用户已经登录，且重复访问登录页
+        showMessage('请勿重复登录', 'warning')
+        // 跳转后台首页
+        next({ path: '/admin/index' })
     } else {
         next()
     }
-    
 })
 
 // 全局路由后置守卫
