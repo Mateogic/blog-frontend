68d68b10efbcb9f37926dcdd6d55ed983710147b
mateogic <ljc_2318516761@163.com>
2025-05-28 13:41:51 +0800

    标签导航栏组件实现：关闭其他、全部标签页

--- src/pages/admin/login.vue
+++ src/pages/admin/login.vue
@@ -55,7 +55,7 @@ import { login } from '@/api/admin/user'
 import { ref, reactive, onMounted, onBeforeUnmount } from 'vue'
 import { useRouter } from 'vue-router'
 import { showMessage } from '@/composables/util'
-import { setToken } from '@/composables/auth'
+import { setToken } from '@/composables/cookie'
 // 登录按钮加载
 const loading = ref(false)
 
