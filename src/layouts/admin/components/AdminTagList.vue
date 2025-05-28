8eff7df7744be81662859c4868103fa8165c9ea9
mateogic <ljc_2318516761@163.com>
2025-05-28 13:09:48 +0800

    标签导航栏路由同步

--- src/layouts/admin/components/AdminTagList.vue
+++ src/layouts/admin/components/AdminTagList.vue
@@ -1,8 +1,10 @@
 <template>
     <!-- 左边：标签导航栏 -->
-    <div class="fixed top-[64px] h-[44px] px-2 right-0 z-50 flex items-center bg-white transition-all duration-300 shadow" :style="{left: menuStore.menuWidth}">
-        <el-tabs v-model="editableTabsValue" type="card" class="demo-tabs" closable @tab-remove="removeTab" style="min-width: 10px;">
-            <el-tab-pane v-for="item in editableTabs" :key="item.name" :label="item.title" :name="item.name">
+    <div class="fixed top-[64px] h-[44px] px-2 right-0 z-50 flex items-center bg-white transition-all duration-300 shadow"
+        :style="{ left: menuStore.menuWidth }">
+        <el-tabs v-model="activeTab" type="card" class="demo-tabs" @tab-remove="removeTab" @tab-change="tabChange" style="min-width: 10px;">
+            <el-tab-pane v-for="item in tabList" :key="item.path" :label="item.title" :name="item.path"
+                :closable="item.path != '/admin/index'">
             </el-tab-pane>
         </el-tabs>
         <!-- 右侧下拉菜单 -->
@@ -27,9 +29,20 @@
 
 <script setup>
 import { ref } from 'vue'
+import { useRoute, useRouter, onBeforeRouteUpdate } from 'vue-router'
+import { setTabList, getTabList } from '@/composables/tag-list'
 
-let tabIndex = 2
-const editableTabsValue = ref('2')
+const route = useRoute()
+const router = useRouter()
+// 当前被选中的 tab
+const activeTab = ref(route.path)
+// 标签页切换事件
+const tabChange = (path) => {
+    // 设置被激活的 Tab 标签
+    activeTab.value = path
+    // 路由跳转
+    router.push(path)
+}
 const editableTabs = ref([
     {
         title: 'Tab 1',
@@ -43,51 +56,85 @@ const editableTabs = ref([
     },
 ])
 
-const addTab = (targetName) => {
-    const newTabName = `${++tabIndex}`
-    editableTabs.value.push({
-        title: 'New Tab',
-        name: newTabName,
-        content: 'New Tab content',
-    })
-    editableTabsValue.value = newTabName
-}
+// 导航栏 tab 数组
+const tabList = ref([
+    {
+        title: '仪表盘',
+        path: "/admin/index"
+    },
+    // {
+    //     title: '文章管理',
+    //     path: "/admin/article/list"
+    // },
+    // {
+    //     title: '分类管理',
+    //     path: "/admin/category/list"
+    // },
+    // {
+    //     title: '标签管理',
+    //     path: "/admin/tag/list"
+    // },
+    // {
+    //     title: '博客设置',
+    //     path: "/admin/blog/setting"
+    // }
+])
+// 删除Tab标签
 const removeTab = (targetName) => {
-    const tabs = editableTabs.value
-    let activeName = editableTabsValue.value
-    if (activeName === targetName) {
-        tabs.forEach((tab, index) => {
-            if (tab.name === targetName) {
-                const nextTab = tabs[index + 1] || tabs[index - 1]
-                if (nextTab) {
-                    activeName = nextTab.name
-                }
-            }
-        })
-    }
 
-    editableTabsValue.value = activeName
-    editableTabs.value = tabs.filter((tab) => tab.name !== targetName)
 }
 import { useMenuStore } from '@/stores/menu'
 
 const menuStore = useMenuStore()
+
+// 在路由切换前被调用
+onBeforeRouteUpdate((to, from) => {
+	// 设置被激活的 Tab 标签
+    activeTab.value = to.path
+    // 添加 Tab 标签页
+    addTab({
+        title: to.meta.title,
+        path: to.path
+    })
+})
+// 添加 Tab 标签页
+function addTab(tab) {
+    // 标签是否不存在
+    let isTabNotExisted = tabList.value.findIndex(item => item.path == tab.path) == -1
+    // 如果不存在
+    if (isTabNotExisted) {
+        // 添加标签
+        tabList.value.push(tab)
+    }
+    // 存储 tabList 到 cookie 中
+    setTabList(tabList.value)
+}
+function initTabList() {
+    // 从 cookie 中获取缓存起来的标签导航栏数据
+    let tabs = getTabList()
+    // 若不为空，则赋值
+    if (tabs) {
+        tabList.value = tabs
+    }
+}
+// 初始化标签导航栏
+initTabList()
 </script>
 
 <style>
 .el-tabs__item {
     font-size: 12px;
-    border: 1px solid #d8dce5!important;
-    border-radius: 3px!important;
+    border: 1px solid #d8dce5 !important;
+    border-radius: 3px !important;
 }
 
 .el-tabs--card>.el-tabs__header .el-tabs__item {
-    margin-left: 0.1rem!important;
-    margin-right: 0.1rem!important;
+    margin-left: 0.1rem !important;
+    margin-right: 0.1rem !important;
 }
 
 .el-tabs__item.is-active {
-    background-color: var(--el-color-primary)!important;
+    background-color: var(--el-color-primary) !important;
     color: #fff;
 }
 
@@ -125,7 +172,8 @@ const menuStore = useMenuStore()
     border: 0;
 }
 
-.el-tabs__nav-prev, .el-tabs__nav-next {
+.el-tabs__nav-prev,
+.el-tabs__nav-next {
     line-height: 35px;
 }
 
@@ -133,5 +181,4 @@ const menuStore = useMenuStore()
     cursor: not-allowed;
     color: #d1d5db;
 }
-
 </style>
