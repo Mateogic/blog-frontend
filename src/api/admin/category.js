f21ddd9ceb17b0d3ab46ca11aa7c9846c32a7db9
mateogic <ljc_2318516761@163.com>
2025-05-31 12:31:27 +0800

    文章管理模块前端开发

--- src/api/admin/category.js
+++ src/api/admin/category.js
@@ -14,3 +14,7 @@ export function deleteCategory(id) {
     return axios.post("/admin/category/delete", {id})
 }
 
+// 获取分类 select 数据
+export function getCategorySelectList() {
+    return axios.post("/admin/category/select/list")
+}
