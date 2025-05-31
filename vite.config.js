d1f35d2ee638ca718b997d781c306d998e3f6f59
mateogic <ljc_2318516761@163.com>
2025-06-01 01:00:00 +0800

    前端工程压缩

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import ViteCompressionPlugin from 'vite-plugin-compression'
+import { visualizer } from 'rollup-plugin-visualizer'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -18,6 +20,13 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
+    ViteCompressionPlugin({
+      filter: /\.(js|css)$/i, // 只压缩 js 和 css 文件
+      threshold: 1024, // 只压缩大于 1KB 的文件
+      algorithm: 'gzip', // 使用 gzip 算法
+      ext: '.gz', // 压缩文件的扩展名
+    }),
+    visualizer() // 放到最后
   ],
   resolve: {
     alias: {
