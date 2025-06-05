b01198aa036584ac6ff995d45c37d35284d0956e
mateogic <ljc_2318516761@163.com>
2025-06-05 20:51:06 +0800

    评论模块后台开发

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,8 @@ import NotFound from '@/pages/frontend/404.vue'
 import AdminWikiList from '@/pages/admin/wiki-list.vue'
 import WikiList from '@/pages/frontend/wiki-list.vue'
 import WikiDetail from '@/pages/frontend/wiki-detail.vue'
+import AdminCommentList from '@/pages/admin/comment-list.vue'
+
 // 统一在这里声明所有路由
 const routes = [
     {
@@ -143,6 +145,13 @@ const routes = [
                 component: AdminWikiList,
                 meta: {
                     title: '知识库管理'
+                }
+            },
+            {
+                path: "/admin/comment/list",
+                component: AdminCommentList,
+                meta: {
+                    title: '评论管理'
                 }
             },
         ]
