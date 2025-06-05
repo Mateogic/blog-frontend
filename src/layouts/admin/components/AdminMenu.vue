941a182cc3d8fbb3f2f6cd172711602c642ebd37
mateogic <ljc_2318516761@163.com>
2025-06-05 13:57:31 +0800

    知识库后台开发

--- src/layouts/admin/components/AdminMenu.vue
+++ src/layouts/admin/components/AdminMenu.vue
@@ -63,9 +63,14 @@ const menus = [
         'path': '/admin/tag/list',
     },
     {
+        'name': '知识库管理',
+        'icon': 'Collection',
+        'path': '/admin/wiki/list',
+    },
+    {
         'name': '博客设置',
         'icon': 'Setting',
-        'path': '/admin/blog/setting',
+        'path': '/admin/blog/settings',
     },
 ]
 </script>
